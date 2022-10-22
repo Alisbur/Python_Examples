3aa0def0afe48d70e7b99e4df2f6d5aea2df6a83
Eugen <mail-4-eugen@yandex.ru>
2022-10-22 08:47:02 +0300

    финальная версия таблицы

--- Homework801 - Task_1/main.py
+++ Homework801 - Task_1/main.py
@@ -19,47 +19,48 @@ while True:
             db_dict = IO_json.read_from_json(DB_JSON_PATH)
 
         case 2:
-            col_name = input("Введите название новой колонки или 0 для отмены: ")
-            if col_name!="0":
+            col_name = input(
+                "Введите название новой колонки или 0 для отмены: ")
+            if col_name != "0":
                 db_dict = table_ops.add_col(db_dict, col_name)
         case 3:
-            col_name = input("Введите название колонки, которую следует удалить или 0 для отмены: ")
-            if col_name!="0":
+            col_name = input(
+                "Введите название колонки, которую следует удалить или 0 для отмены: ")
+            if col_name != "0":
                 db_dict = table_ops.del_col(db_dict, col_name)
 
         case 4:
-            new_row_dict = {}
-            if db_dict:
-                new_row_key = int(max(db_dict.keys()))+1
-
-                for key in db_dict[next(iter(db_dict))].keys():
-                    new_value = input(f"Введите значение поля {key} или 0 для отмены: ")
-                    if new_value == "0":
-                        break
-                    new_row_dict[key] = new_value
-                if new_value != "0":
-                    db_dict = table_ops.add_row(db_dict, new_row_key, new_row_dict)
-            else:
-                print("Прежде чем создавать записи создайте хотя бы одну колонку.")
+            db_dict = table_ops.add_row(db_dict)
         case 5:
-            row_to_modify = input("Введите номер записи, которую следует изменить, 0 для отмены: ")
-            if row_to_modify!="0":
-                db_dict = table_ops.mod_row(db_dict, row_to_modify)
+            while True:
+                row_to_modify = input(
+                    "Введите ключ записи, которую следует изменить, 0 для отмены: ")
+                if row_to_modify.isdigit():
+                    break
+                else:
+                    print("Ошибка ввода! Номер должен быть числом.\n")
+            if row_to_modify != "0":
+                db_dict = table_ops.mod_row(db_dict, int(row_to_modify))
         case 6:
-            row_to_del = input("Введите номер записи, которую следует удалить, 0 для отмены: ")
-            if row_to_del!="0":
-                db_dict = table_ops.del_row(db_dict, row_to_del)
+            while True:
+                row_to_del = input(
+                    "Введите ключ записи, которую следует удалить, 0 для отмены: ")
+                if row_to_del.isdigit():
+                    break
+                else:
+                    print("Ошибка ввода! Номер должен быть числом.\n")
+            if row_to_del != "0":
+                db_dict = table_ops.del_row(db_dict, int(row_to_del))
 
         case 7:
             db_dict = table_ops.sort_table_key(db_dict)
         case 8:
-            col_name = input("Введите название колонки по которой будет произведена сортировка или 0 для отмены: ")
-            if col_name!="0":
+            col_name = input(
+                "Введите название колонки по которой будет произведена сортировка или 0 для отмены: ")
+            if col_name != "0":
                 db_dict = table_ops.sort_table_val(db_dict, col_name)
 
         case 9:
             IO_json.append_to_json(DB_JSON_PATH, db_dict)
 
     db_print.db_print(db_dict)
-
-
--- Homework801 - Task_1/table_ops.py
+++ Homework801 - Task_1/table_ops.py
@@ -1,11 +1,12 @@
 import time
 
+
 def add_col(db_dict, col):
     if db_dict:
         for key in db_dict.keys():
             db_dict[key][col] = None
     else:
-        db_dict[1] = {col : None}
+        db_dict[1] = {col: None}
     print(f"\nВ таблицу добавлена колонка {col}.\n")
     time.sleep(1)
     return db_dict
@@ -25,47 +26,69 @@ def del_col(db_dict, col):
     return db_dict
 
 
-def add_row(db_dict, key, new_row):
-    if key in db_dict.keys():
-        print(f"\nВ таблице уже имеется строка с индексом {key}. Ничего не изменилось.\n")
+def add_row(db_dict):
+    new_row_dict = {}
+    if db_dict:
+        new_row_key = max(db_dict.keys())+1
+
+        for key in db_dict[next(iter(db_dict))].keys():
+            new_value = input(
+                f"Введите значение поля {key} или 0 для отмены: ")
+            if new_value == "0":
+                break
+            new_row_dict[key] = new_value
+
+        if new_value != "0":
+            db_dict[new_row_key] = new_row_dict
+            print(f"\nВ таблицу добавлена строка с ключом {key}.\n")
     else:
-        db_dict[key] = new_row
-        print(f"\nВ таблицу добавлена строка с индексом {key}.\n")
+        print("Прежде чем создавать записи создайте хотя бы одну колонку.")
     time.sleep(1)
     return db_dict
 
 
 def mod_row(db_dict, key):
     while True:
-        new_key = input(f"Индекс строки {key}. Введите новый индекс или нажмите Ввод.\n")
-        if new_key != "":
-            if int(new_key) not in list(db_dict.keys()):
+        new_key_str = input(
+            f"Ключ строки {key}. Введите новый ключ или нажмите Ввод.\n")
+        if not new_key_str.isdigit() and new_key_str != "":
+            print(f"\nВведённый ключ не является числом. Используйте другой.\n")
+            time.sleep(1)
+            continue
+        if new_key_str != "":
+            new_key = int(new_key_str)
+            if new_key not in list(db_dict.keys()):
                 break
             else:
-                print(f"\nВведённый ключ уже используется в таблице. Используйте другой.\n")
+                print(
+                    f"\nВведённый ключ уже используется в таблице. Используйте другой.\n")
+                time.sleep(1)
         else:
-            new_key=key
+            new_key = key
             break
-    if new_key != key:
-        db_dict[int(new_key)] = db_dict[int(key)]
 
-    for el in db_dict[int(key)].keys():
-        new_val = input(f"Значение {el} равно {db_dict[int(key)][el]}. Введите новое значение или нажмите Ввод.\n")
-        if new_val != "":
-            db_dict[int(new_key)][el] = new_val
     if new_key != key:
-        db_dict.pop(int(key))
+        db_dict[new_key] = db_dict[key]
+
+    for el in db_dict[key].keys():
+        new_val = input(
+            f"Значение {el} равно {db_dict[key][el]}. Введите новое значение или нажмите Ввод.\n")
+        if new_val != "":
+            db_dict[new_key][el] = new_val
+    if new_key != key:
+        db_dict.pop(key)
     print(f"\nИзменения внесены в запись.\n")
     time.sleep(1)
     return db_dict
 
 
 def del_row(db_dict, key):
-    if int(key) in list(db_dict.keys()):
-        db_dict.pop(int(key))
-        print(f"\nСтрока с индексом {key} удалена.\n")
+    if key in list(db_dict.keys()):
+        db_dict.pop(key)
+        print(f"\nСтрока с ключом {key} удалена.\n")
     else:
-        print(f"\nВ таблице отсутствует строка с индексом {key}. Удаление невозможно.\n")
+        print(
+            f"\nВ таблице отсутствует строка с ключом {key}. Удаление невозможно.\n")
     time.sleep(1)
     return db_dict
 
@@ -73,28 +96,28 @@ def del_row(db_dict, key):
 def sort_table_key(db_dict):
     if db_dict:
         sorted_tuple = sorted(db_dict.items(), key=lambda x: x[0])
-        print("Строки отсортированы по индексам.")
-        time.sleep(1)           
+        print("Строки отсортированы по индексам ключей.")
+        time.sleep(1)
         return dict(sorted_tuple)
-    print("В таблице нет данных для сортировки")    
-    time.sleep(1)     
+    print("В таблице нет данных для сортировки")
+    time.sleep(1)
     return db_dict
 
 
 def sort_table_val(db_dict, col_name):
     if db_dict:
-        reverse_sort = False 
+        reverse_sort = False
         if input("Для сортировки по возрастанию нажмите Ввод. Для сортировки по убыванию введите любое значение: "):
             reverse_sort = True
         if col_name in list(db_dict[list(db_dict.keys())[0]].keys()):
-            sorted_tuple = sorted(db_dict.items(), key=lambda x: x[1][col_name], reverse = reverse_sort)
-            print(f"\nТаблица отсортирована по {'возрастанию' if not reverse_sort else 'убыванию'} значений в столбце {col_name}.\n")
-            time.sleep(1)       
+            sorted_tuple = sorted(
+                db_dict.items(), key=lambda x: x[1][col_name], reverse=reverse_sort)
+            print(
+                f"\nТаблица отсортирована по {'возрастанию' if not reverse_sort else 'убыванию'} значений в столбце {col_name}.\n")
+            time.sleep(1)
             return dict(sorted_tuple)
         else:
-            print("В таблице нет указанной колонки. Данные не были отсортированы.")                
-    print("В таблице нет данных для сортировки")    
-    time.sleep(1)     
+            print("В таблице нет указанной колонки. Данные не были отсортированы.")
+    print("В таблице нет данных для сортировки")
+    time.sleep(1)
     return db_dict
-
-    
