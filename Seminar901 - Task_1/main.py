217fd8077973251e545f837cd3855ee927c66af6
Alisbur <110252416+Alisbur@users.noreply.github.com>
2022-10-24 14:32:02 +0300

    Update main.py

--- Seminar901 - Task_1/main.py
+++ Seminar901 - Task_1/main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
 
-updater = Updater('5529264334:AAG7goYxffk1H69rmm5IbBXAOL7_iAf_o-U')
+updater = Updater('')
 
 updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
